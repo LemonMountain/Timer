dffcdff4b6aefa6bc6dfe942c554c2c96f15286e
mj2 <mj2@mj2sAir.local>
2021-06-29 00:02:45 +0900

    this is time bomb

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-var date = new Date();
-// 날짜 객체
-var hour = date.getHours;
-//시간 객체
-var minute = date.getMinutes;
-//분 객체
-var second = date.getSeconds;
-//초 객체
+function setTime() {
+  var now = new Date();
+  var time = [];
+  var arrows = document.querySelectorAll('.arrow');
+  var dividers = document.querySelectorAll('.divider');
+  
+  time.push(now.getHours());
+  time.push(now.getMinutes());
+  time.push(now.getSeconds());
+  time.push(Math.floor(now.getMilliseconds() / 10));
+  
+  time = time.map( elem => elem < 10 ? '0' + elem : elem );
+  
+  for (var index = 0; index < arrows.length; index++) {
+    arrows[index].innerHTML = time[index];
+  }
+
+  dividers.forEach( elem => {
+    if (time[3] < 50) {
+      elem.style.color = 'black';
+    } else {
+      elem.style.color = 'red';
+    }
+  });
+}
+
+setInterval(setTime, 10);
